943ac2595aaedaf466f8013b32aad5017652e25d
Luca Sepe <luca.sepe@gmail.com>
2022-09-08 14:31:55 +0200

    add patch to handle baseUrls with extra path

--- pkg/clients/github/client.go
+++ pkg/clients/github/client.go
@@ -1,13 +1,9 @@
 package github
 
 import (
-	"context"
-	"errors"
 	"fmt"
 	"net/http"
-
-	"github.com/carlmjohnson/requests"
-	"github.com/krateoplatformops/provider-github/apis/repo/v1alpha1"
+	"net/url"
 )
 
 const (
@@ -38,119 +34,36 @@ type ClientOpts struct {
 
 // Client is a tiny Github client
 type Client struct {
-	apiUrl     string
-	httpClient *http.Client
-	repos      *RepoService
+	apiUrl       string
+	apiExtraPath string
+	httpClient   *http.Client
+	repos        *RepoService
 }
 
 // NewClient returns a new Github Client
 func NewClient(opts ClientOpts) *Client {
 	res := &Client{
-		apiUrl:     defaultApiURL,
-		httpClient: opts.HttpClient,
+		apiUrl:       defaultApiURL,
+		apiExtraPath: "",
+		httpClient:   opts.HttpClient,
 	}
 
 	if len(opts.ApiURL) > 0 {
-		res.apiUrl = opts.ApiURL
+		u, err := url.ParseRequestURI("http://github.insiel.it/api/v3")
+		if err != nil {
+			res.apiUrl = opts.ApiURL
+			res.apiExtraPath = ""
+		} else {
+			res.apiUrl = fmt.Sprintf("%s://%s", u.Scheme, u.Host)
+			res.apiExtraPath = u.Path
+		}
 	}
 
-	res.repos = newRepoService(res.httpClient, res.apiUrl, opts.Token)
+	res.repos = newRepoService(res.httpClient, res.apiUrl, res.apiExtraPath, opts.Token)
 
 	return res
 }
 
 func (c *Client) Repos() *RepoService {
 	return c.repos
-}
-
-// RepoService provides methods for creating and reading repositories.
-type RepoService struct {
-	client *http.Client
-	apiUrl string
-	token  string
-}
-
-// newRepoService returns a new RepoService.
-func newRepoService(httpClient *http.Client, apiUrl, token string) *RepoService {
-	return &RepoService{
-		client: httpClient,
-		apiUrl: apiUrl,
-		token:  token,
-	}
-}
-
-func (s *RepoService) Create(opts *v1alpha1.RepoParams) error {
-	ok, err := s.isOrg(opts.Org)
-	if err != nil {
-		return err
-	}
-
-	pt := "/user/repos"
-	if ok {
-		pt = fmt.Sprintf("orgs/%s/repos", opts.Org)
-	}
-
-	githubError := &GithubError{}
-
-	err = requests.URL(s.apiUrl).Path(pt).
-		Client(s.client).
-		Method(http.MethodPost).
-		Header("Authorization", fmt.Sprintf("token %s", s.token)).
-		BodyJSON(map[string]interface{}{
-			"name":      opts.Name,
-			"private":   opts.Private,
-			"auto_init": true,
-		}).
-		AddValidator(ErrorJSON(githubError, 201)).
-		Fetch(context.Background())
-	if err != nil {
-		var gerr *GithubError
-		if errors.As(err, &gerr) {
-			return fmt.Errorf(gerr.Error())
-		}
-		return err
-	}
-
-	return nil
-}
-
-// Get fetches a repository.
-//
-// GitHub API docs: https://docs.github.com/en/free-pro-team@latest/rest/reference/repos/#get-a-repository
-func (s *RepoService) Exists(opts *v1alpha1.RepoParams) (bool, error) {
-	pt := fmt.Sprintf("repos/%s/%s", opts.Org, opts.Name)
-
-	err := requests.URL(s.apiUrl).Path(pt).
-		Client(s.client).
-		Method(http.MethodGet).
-		Header("Authorization", fmt.Sprintf("token %s", s.token)).
-		CheckStatus(200).
-		Fetch(context.Background())
-	if err != nil {
-		if requests.HasStatusErr(err, 404) {
-			return false, nil
-		}
-
-		return false, err
-	}
-
-	return true, nil
-}
-
-func (s *RepoService) isOrg(owner string) (bool, error) {
-	err := requests.URL(s.apiUrl).Pathf("/orgs/%s", owner).
-		Client(s.client).
-		Method(http.MethodGet).
-		Header("Authorization", fmt.Sprintf("token %s", s.token)).
-		CheckStatus(200).
-		Fetch(context.Background())
-	if err != nil {
-		if requests.HasStatusErr(err, 404) {
-			return false, nil
-		}
-
-		return false, err
-	}
-
-	return true, nil
 }
---
+++ pkg/clients/github/repos.go
@@ -0,0 +1,107 @@
+package github
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"path"
+
+	"github.com/carlmjohnson/requests"
+	"github.com/krateoplatformops/provider-github/apis/repo/v1alpha1"
+)
+
+// RepoService provides methods for creating and reading repositories.
+type RepoService struct {
+	client       *http.Client
+	apiUrl       string
+	apiExtraPath string
+	token        string
+}
+
+// newRepoService returns a new RepoService.
+func newRepoService(httpClient *http.Client, apiUrl, extraPath, token string) *RepoService {
+	return &RepoService{
+		client:       httpClient,
+		apiUrl:       apiUrl,
+		apiExtraPath: extraPath,
+		token:        token,
+	}
+}
+
+func (s *RepoService) Create(opts *v1alpha1.RepoParams) error {
+	ok, err := s.isOrg(opts.Org)
+	if err != nil {
+		return err
+	}
+
+	pt := path.Join(s.apiExtraPath, "/user/repos")
+	if ok {
+		pt = path.Join(s.apiExtraPath, fmt.Sprintf("orgs/%s/repos", opts.Org))
+	}
+
+	githubError := &GithubError{}
+
+	err = requests.URL(s.apiUrl).Path(pt).
+		Client(s.client).
+		Method(http.MethodPost).
+		Header("Authorization", fmt.Sprintf("token %s", s.token)).
+		BodyJSON(map[string]interface{}{
+			"name":      opts.Name,
+			"private":   opts.Private,
+			"auto_init": true,
+		}).
+		AddValidator(ErrorJSON(githubError, 201)).
+		Fetch(context.Background())
+	if err != nil {
+		var gerr *GithubError
+		if errors.As(err, &gerr) {
+			return fmt.Errorf(gerr.Error())
+		}
+		return err
+	}
+
+	return nil
+}
+
+// Get fetches a repository.
+//
+// GitHub API docs: https://docs.github.com/en/free-pro-team@latest/rest/reference/repos/#get-a-repository
+func (s *RepoService) Exists(opts *v1alpha1.RepoParams) (bool, error) {
+	pt := path.Join(s.apiExtraPath, fmt.Sprintf("repos/%s/%s", opts.Org, opts.Name))
+
+	err := requests.URL(s.apiUrl).Path(pt).
+		Client(s.client).
+		Method(http.MethodGet).
+		Header("Authorization", fmt.Sprintf("token %s", s.token)).
+		CheckStatus(200).
+		Fetch(context.Background())
+	if err != nil {
+		if requests.HasStatusErr(err, 404) {
+			return false, nil
+		}
+
+		return false, err
+	}
+
+	return true, nil
+}
+
+func (s *RepoService) isOrg(owner string) (bool, error) {
+	pt := path.Join(s.apiExtraPath, fmt.Sprintf("/orgs/%s", owner))
+	err := requests.URL(s.apiUrl).Path(pt).
+		Client(s.client).
+		Method(http.MethodGet).
+		Header("Authorization", fmt.Sprintf("token %s", s.token)).
+		CheckStatus(200).
+		Fetch(context.Background())
+	if err != nil {
+		if requests.HasStatusErr(err, 404) {
+			return false, nil
+		}
+
+		return false, err
+	}
+
+	return true, nil
+}
